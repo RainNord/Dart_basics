4d3fafb79891b0dcbc62b96fab1f6eb8b8301123
RainNord <110254472+RainNord@users.noreply.github.com>
2022-07-31 02:09:04 +0600

    Задание 2
    Задание 2

---
+++ lib/transfer_system.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+extension TransferSystem on int {
+  int decimalToBinary(){
+    List<int> listBinary = [];
+    int a = this;
+    int b;
+    while(a>0){
+      b=a;
+      a~/=2;
+      b%=2;
+      listBinary.add(b);
+    }
+    return int.parse(listBinary.reversed.join(""));
+  }
+
+  int binaryToDecimal(){
+    String strBinary  = this.toString();
+    num result = 0;
+    for(int i=strBinary.length-1; i>=0; i--){   
+      result += pow(2,i)*num.parse(strBinary[strBinary.length-i-1]); 
+    }
+    return result.toInt();
+  }
+}
