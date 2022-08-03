2d9d1f9f7a073ffc1beda248e83b1e04ff202a16
RainNord <110254472+RainNord@users.noreply.github.com>
2022-08-03 11:11:00 +0600

    Update
    Задание 7

--- lib/degree_root.dart
+++ lib/degree_root.dart
@@ -1,20 +1,24 @@
-extension RootDegree on num {
+extension RootExponent on num {
+
+  double calcRootExponent(int n, double eps){
+    if((this > 0)&(n > 0)){
+      double num=toDouble();
+      double x=num/n;
+      double a=num;
+
+      while(_myAbs(x - a) >= eps){
+          a=num;
+          for(int i = 1; i < n; i++){
+              a/=x;
+          }
+          x=(1/n)*(((n-1)*x)+a);
+      }
+
+      return x; 
+    }else{
+      throw ArgumentError('Number and exponent must be >0');
+    }
+  }
 
   double _myAbs(double x){ return (x < 0)? -x : x; }
-
-  double calcRootDegree(int rootDegree, double accurasy){
-    double a=toDouble();
-    double root=a/rootDegree;
-    double x=a;
-
-    while(_myAbs(root - x) >= accurasy){
-        x=a;
-        for(int i = 1; i < rootDegree; i++){
-            x=x/root;
-        }
-        root=0.5*(x+root);
-    }
-
-    return root; 
-  }
 }
